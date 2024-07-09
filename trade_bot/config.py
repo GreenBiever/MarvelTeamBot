ba168f1a987356666ddf3380cbe32f82ec127a7d
Vitaly312 <vn264537@gmail.com>
2024-07-09 16:39:27 +0300

    Merge OXK-site with trade_bot

--- trade_bot/config.py
+++ trade_bot/config.py
@@ -1,2 +1,5 @@
-TOKEN = ''
+TOKEN = '6284731506:AAFdUBT3dzcnvROLsWlGi0m491-f4mvtbdo'
 SQLALCHEMY_URL = 'sqlite+aiosqlite:///trade.db'
+WEBHOOK_URL = 'https://59835570-9cca-4616-bc71-c3d77f92a848.tunnel4.com'
+WEBHOOK_PORT = 8080
+TELEGRAM_WEBHOOK_PATH = '/telegram_webhook/'
--- trade_bot/main.py
+++ trade_bot/main.py
@@ -1,20 +1,47 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.exceptions import TelegramBadRequest
 import config
-import asyncio
 from handlers import main_handlers, wallet_handlers
 from database.connect import init_models, dispose_engine
 import logging
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi.staticfiles import StaticFiles
+from site_backend import routers as api_router
+import uvicorn
 
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher()
-dp.include_routers(main_handlers.router, wallet_handlers.router)
+dp.include_routers(main_handlers.router, 
+                   wallet_handlers.router
+                   )
 
-async def main():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await bot.set_webhook(url = (config.WEBHOOK_URL
+                           + config.TELEGRAM_WEBHOOK_PATH))
+    bot_info = await bot.get_me()
+    logging.getLogger(__name__).info(f'Бот успешно запущен: {bot_info.username}')
     await init_models()
-    await dp.start_polling(bot)
+    yield
+    await bot.delete_webhook()
+    await bot.close()
     await dispose_engine()
 
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router.router)
+app.mount("/exchange", StaticFiles(directory="okx/ru/exchange"), name="static")
+
+
+@app.post(config.TELEGRAM_WEBHOOK_PATH)
+async def bot_webhook(update: dict):
+    telegram_update = types.Update(**update)
+    try:
+        await dp.feed_update(bot=bot, update=telegram_update)
+    except TelegramBadRequest as e:
+        logging.error(e, stack_info=True)
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    uvicorn.run(app, host="0.0.0.0", port=config.WEBHOOK_PORT)
